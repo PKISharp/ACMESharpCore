7394b0fb6fc0cccf1126a4a852e0d3d572b7377d
Eugene Bekker <ebekker@users.noreply.github.com>
2018-05-02 12:43:47 -0400

    adding integration tests project

---
+++ test/ACMESharp.Core.IntegrationTests/_Assembly.cs
@@ -0,0 +1,13 @@
+
+using ACMESharp.Testing.Xunit;
+using Xunit;
+
+[assembly: TestCaseOrderer(TestOrderer.TypeName, TestOrderer.AssemblyName)]
+[assembly: TestCollectionOrderer(TestOrderer.TypeName, TestOrderer.AssemblyName)]
+
+[assembly: CollectionBehavior(
+    CollectionBehavior.CollectionPerClass
+  //CollectionBehavior.CollectionPerAssembly
+  //,MaxParallelThreads = n
+    ,DisableTestParallelization = true
+)]
