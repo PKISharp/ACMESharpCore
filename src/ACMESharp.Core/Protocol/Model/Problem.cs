0e3c3d8c8c29e736ede46780070769a59d0ac9fd
Eugene Bekker <ebekker@users.noreply.github.com>
2018-05-04 08:44:40 -0400

    filling out problem details

--- src/ACMESharp.Core/Protocol/Model/Problem.cs
+++ src/ACMESharp.Core/Protocol/Model/Problem.cs
@@ -5,6 +5,16 @@ namespace ACMESharp.Protocol.Model
 {
     public class Problem
     {
+        public const string StandardProblemTypeNamespace = "urn:ietf:params:acme:error:";
 
+        [JsonProperty("type", Required = Required.Always)]
+        [Required]
+        public string Type { get; set; }
+
+        [JsonProperty("detail")]
+        public string Detail { get; set; }
+
+        [JsonProperty("status")]
+        public int? Status { get; set; }
     }
 }
