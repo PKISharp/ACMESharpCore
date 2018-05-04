244839029c24caefde58af715e6e6339e77a8454
Eugene Bekker <ebekker@users.noreply.github.com>
2018-05-04 09:09:56 -0400

    handling some LE-specific message elements

--- src/ACMESharp.Core/Protocol/Messages/CreateAccountResponse.cs
+++ src/ACMESharp.Core/Protocol/Messages/CreateAccountResponse.cs
@@ -20,7 +20,11 @@ namespace ACMESharp.Protocol.Messages
 
 
         // TODO: are these standard or specific to LE?
+        //    "agreement": "https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf",
+        //    "initialIp": "50.235.30.49",
+        //    "createdAt": "2018-05-02T22:23:30Z",
         public string InitialIp { get; set; }
         public string CreatedAt { get; set; }
+        public string Agreement { get; set; }
     }
 }
