ea0e8fafa2e40b3192a3def1f1d0258ca861656a
Eugene Bekker <ebekker@users.noreply.github.com>
2018-06-05 14:15:42 -0400

    docs

--- src/ACMESharp.Core/Protocol/Model/AccountDetails.cs
+++ src/ACMESharp.Core/Protocol/Model/AccountDetails.cs
@@ -1,5 +1,14 @@
 namespace ACMESharp.Protocol.Model
 {
+    /// <summary>
+    /// An aggregation of Account details including resource payload and ancillary,
+    /// associated data.
+    /// </summary>
+    /// <remarks>
+    /// This represents a superset of details that are included in responses
+    /// to several ACME operations regarding an ACME Account, such as Account
+    /// registration, update, key rotation and deactivation.
+    /// </remarks>
     public class AccountDetails
     {
         public Account Payload { get; set; }
--- src/ACMESharp.Core/Protocol/Model/OrderDetails.cs
+++ src/ACMESharp.Core/Protocol/Model/OrderDetails.cs
@@ -1,5 +1,14 @@
 namespace ACMESharp.Protocol.Model
 {
+    /// <summary>
+    /// An aggregation of Order details including resource payload and ancillary,
+    /// associated data.
+    /// </summary>
+    /// <remarks>
+    /// This represents a superset of details that are included in responses
+    /// to several ACME operations regarding an ACME Order, such as 
+    /// Order creation and finalization.
+    /// </remarks>
     public class OrderDetails
     {
         public Order Payload { get; set; }
