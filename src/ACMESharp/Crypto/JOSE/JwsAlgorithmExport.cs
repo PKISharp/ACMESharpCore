a69fa7e9df6b3166ab737db568847a67c1f8a902
Glatzer, Thomas <glatzert@uni-mainz.de>
2018-07-11 13:00:50 +0200

    Renames and consolidation

---
+++ src/ACMESharp/Crypto/JOSE/JwsAlgorithmExport.cs
@@ -0,0 +1,10 @@
+namespace ACMESharp.Crypto.JOSE
+{
+
+    public class JwsAlgorithmExport
+    {
+        public string Algorithm { get; set; }
+
+        public string PrivateKey { get; set; }
+    }
+}
--- src/ACMESharp/Crypto/JOSE/JwsExport.cs
+++
@@ -1,9 +0,0 @@
-namespace ACMESharp.Crypto.JOSE
-{
-    public class JwsExport
-    {
-        public string JwsAlgorithm { get; set; }
-
-        public string JwsParameters { get; set; }
-    }
-}
