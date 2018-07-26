e01815cea2af6bb048ac4d4330c0cf65ec171590
Eugene Bekker <ebekker@users.noreply.github.com>
2018-07-26 13:09:43 -0400

    adding alternative possibility

--- src/ACMESharp.MockServer/ProtectedHeader.cs
+++ src/ACMESharp.MockServer/ProtectedHeader.cs
@@ -6,6 +6,8 @@ namespace ACMESharp.MockServer
     {
         public string Alg { get; set; }
 
+        public string Kid { get; set; }
+
         public string Url { get; set; }
 
         public string Nonce { get; set; }
