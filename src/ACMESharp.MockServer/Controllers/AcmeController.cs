fa67abb3180a376d10070dc95ecdcb24793503e8
WouterTinus <wouter.tinus@gmail.com>
2019-02-09 11:56:47 +0100

    Revert "Revocation support"
    This reverts commit ed3097e901f6f49bfcde27b62ba0695262da00ac.

--- src/ACMESharp.MockServer/Controllers/AcmeController.cs
+++ src/ACMESharp.MockServer/Controllers/AcmeController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -392,30 +392,6 @@ namespace ACMESharp.MockServer.Controllers
             return dbCert.Pem;
         }
 
-        // "revoke-cert": "https://tools.ietf.org/html/draft-ietf-acme-acme-18#section-7.6"
-        [HttpPost("revoke-cert")]
-        public ActionResult<bool> Revoke(string acctId,
-            [FromBody]JwsSignedPayload signedPayload)
-        {
-            if (!int.TryParse(acctId, out var acctIdNum))
-                return NotFound();
-         
-            var ph = ExtractProtectedHeader(signedPayload);
-
-            ValidateNonce(ph);
-
-            var acct = _repo.GetAccountByKid(ph.Kid);
-            if (acct == null)
-                throw new Exception("could not resolve account");
-
-            ValidateAccount(acct, signedPayload);
-
-            // TODO: do stuff? 
-
-            GenerateNonce();
-            return true;
-        }
-
         // "https://acme-staging-v02.api.letsencrypt.org/acme/authz/740KRMwcT0UrLXdUKOlgMnfNbzpSQtRaWjbyA1UgIJ4",
         /*
         // OK
@@ -548,7 +524,6 @@ namespace ACMESharp.MockServer.Controllers
 
             return dbChlng.Payload;
         }
-
 
         T ExtractPayload<T>(JwsSignedPayload signedPayload)
         {
